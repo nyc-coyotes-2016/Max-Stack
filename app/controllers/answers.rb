f67197b1af2893ef927a8eacd443989c8da75e25
Catherine Khuu <sunshine.dumbo@gmail.com>
2016-11-03 18:05:32 -0400

    Start work on edit answers.

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -5,6 +5,7 @@ get '/questions/:id/answers/new' do
 end
 
 post '/questions/:id/answers' do
+  require_login
   answer = Answer.new(params[:answer])
   if answer && answer.update(question_id: params[:id], responder_id: current_user.id)
     status 200
@@ -14,5 +15,25 @@ post '/questions/:id/answers' do
     @question = Question.find_by(id: params[:id])
     @errors = answer.errors.full_messages
     erb :'/answers/new'
+  end
+end
+
+get '/questions/:id/answers/:answer_id/edit' do
+  require_login
+  @question = Question.find_by(id: params[:id])
+  @answer = Answer.find_by(id: params[:answer_id])
+  erb :'/answers/edit'
+end
+
+put '/questions/:id/answers/:answer_id' do
+  @question = Question.find_by(id: params[:id])
+  @answer = Answer.find_by(id: params[:answer_id])
+  if @answer && @answer.update(params[:answer])
+    status 200
+    redirect "/questions/#{@question.id}"
+  else
+    status 400 
+    @errors = @answer.errors.full_messages
+    erb :'/answers/edit'
   end
 end
