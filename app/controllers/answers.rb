ba37ccc427fe5089828bb3804ced24521bccc0c6
Catherine Khuu <sunshine.dumbo@gmail.com>
2016-11-03 16:15:42 -0400

    Create answer route

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,2 +1,17 @@
 get '/questions/:id/answers/new' do
-end 
+  require_login
+  erb :'/answers/new'
+end
+
+post 'questions/:id/answers' do
+  @answer = Answer.new(params[:answer])
+  if @answer && @answer.update(question_id: params[:id], responder_id: current_user.id)
+    status 200
+    redirect "/questions/#{params[:id]}"
+  else
+    status 400
+    @errors = @answer.errors.full_messages
+    erb :'/answers/new'
+  end
+
+end
