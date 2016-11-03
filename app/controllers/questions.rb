3fd3bbe32e8ec138180ce1ade9bae3da7c31ae62
Catherine Khuu <sunshine.dumbo@gmail.com>
2016-11-03 15:49:50 -0400

    Create restful questions routes, questions show, new, and _errors erbs.

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -3,6 +3,23 @@ get '/questions' do
   erb :index
 end
 
+post '/questions' do
+  @question = Question.new(params[:question])
+  @question.update(creator_id: current_user.id)
+  if @question && @question.save
+    status 200
+    redirect "/questions/#{@question.id}"
+  else
+    status 400
+    @errors = @question.errors.full_messages
+    erb :'/questions/new'
+  end
+end
+
+get '/questions/new' do
+  erb :'/questions/new'
+end
+
 get '/questions/:id' do
   @question = Question.find_by(id: params[:id])
   erb :'/questions/show'
